a4dbeb7a1002d52c92b8be9655fdc9ee5bf71eaa
fredzstevanato <fredzstevanato@gmail.com>
2022-11-16 20:00:18 -0300

    connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const PORT = 3333;
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const PORT = 3333;
+    const app = express();
 
-const app = express();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => console.log('Error connecting to mongo'));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
